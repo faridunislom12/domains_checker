345381240056232aafb4982e158a886251cc7b5d
Muhamadiev Faridun <you@example.com>
2025-04-11 18:57:37 +0500

    keep results in memory

--- lib/domain_checker/worker.ex
+++ lib/domain_checker/worker.ex
@@ -6,16 +6,20 @@ defmodule DomainChecker.Worker do
   end
 
   def init(domain) do
-    {:ok, domain, {:continue, :check_loop}}
+    {:ok, {domain, []}, {:continue, :check_loop}}
   end
 
-  def handle_continue(:check_loop, domain) do
+  def handle_continue(:check_loop, {domain, results}) do
     result = check_domain(domain)
-    log_results(domain, result)
 
     :timer.sleep(10_000)
 
-    {:noreply, domain, {:continue, :check_loop}}
+    timestamp = DateTime.utc_now() |> DateTime.to_iso8601()
+    updated_results = results ++ [{timestamp, result}]
+
+    log_results(domain, updated_results)
+
+    {:noreply, {domain, updated_results}, {:continue, :check_loop}}
   end
 
   # --- Helpers ---
@@ -27,8 +31,22 @@ defmodule DomainChecker.Worker do
         1
 
       {:error, reason} ->
-        #log_results(domain, reason)
+        #log_result(domain, reason)
         0
+    end
+  end
+
+  defp log_result(domain, result) do
+    log_dir = "logs/checker"
+    File.mkdir_p!(log_dir)
+
+    log_file = Path.join(log_dir, "#{domain}.log")
+    timestamp = DateTime.utc_now() |> DateTime.to_string()
+    line = "#{timestamp} — #{result}\n"
+
+    case File.write(log_file, line, [:append]) do
+      :ok -> :ok
+      {:error, reason} -> IO.puts("[ERROR] Failed to write log: #{inspect(reason)}")
     end
   end
 
@@ -37,8 +55,14 @@ defmodule DomainChecker.Worker do
     File.mkdir_p!(log_dir)
 
     log_file = Path.join(log_dir, "#{domain}.log")
-    timestamp = DateTime.utc_now() |> DateTime.to_string()
-    line = "#{timestamp} — #{result}\n"
+
+    result_string =
+      Enum.map(result, fn {timestamp, res} ->
+        "#{timestamp} — #{res}"
+      end)
+      |> Enum.join("\n")  # Join entries with newline if multiple
+
+    line = "#{result_string}\n\n"
 
     case File.write(log_file, line, [:append]) do
       :ok -> :ok
